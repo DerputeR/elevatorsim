0eedfb1ddb05716b76cf24d88af6060fbcc898cd
DerputeR <dekxy-developer@yahoo.com>
2025-06-08 00:23:38 -0400

    feat: added untested look and scan (iterative) algorithms

--- src/elevator.cpp
+++ src/elevator.cpp
@@ -1,5 +1,14 @@
 #include "elevator.h"
 
+Direction operator-(Direction& d) {
+    switch (d) {
+    case Direction::Up:
+        return Direction::Down;
+    case Direction::Down:
+        return Direction::Up;
+    }
+}
+
 bool Elevator::call_floor(int floor) {
     if (floor <= max_floor && floor >= min_floor) {
         floors_called[floor] = true;
@@ -12,9 +21,9 @@ bool Elevator::move_to_floor(int floor) {
     if (floor <= max_floor && floor >= min_floor) {
         floors_called[floor] = false;
         if (floor < current_floor) {
-            move_dir = Direction::down;
+            move_dir = Direction::Down;
         } else if (floor > current_floor) {
-            move_dir = Direction::up;
+            move_dir = Direction::Up;
         }
         current_floor = floor;
         return true;
@@ -23,17 +32,54 @@ bool Elevator::move_to_floor(int floor) {
 }
 
 void single_scan(Elevator& e) {
+    // check if there are any called floors in the current direction
+    int next_floor = find_next_floor_in_direction(e, e.move_dir);
 
+    // if not, try the opposite direction only if we're at the limits
+    if (next_floor == -1) {
+        if (e.current_floor == (e.move_dir == Direction::Down ? e.min_floor : e.max_floor)) {
+            next_floor == find_next_floor_in_direction(e, -e.move_dir);
+        }
+    }
+
+    // if there are no more floors to service, exit early
+    if (next_floor == -1) {
+        e.stopped = true;
+        return;
+    }
+    else {
+        e.next_floor = next_floor;
+    }
+}
+
+static int find_next_floor_in_direction(const Elevator& e, const Direction dir) {
+    int next_floor = -1;
+    for (int i = e.current_floor;
+            dir == Direction::Up ? i <= e.max_floor : i >= e.min_floor;
+            i++) {
+        if (e.floors_called[i]) {
+            next_floor = e.floors_called[i];
+            break;
+        }
+    }
+    return next_floor;
 }
 
 void single_look(Elevator& e) {
+    // check if there are any called floors in the current direction
+    int next_floor = find_next_floor_in_direction(e, e.move_dir);
 
-}
+    // if not, try the opposite direction
+    if (next_floor == -1) {
+        next_floor == find_next_floor_in_direction(e, -e.move_dir);
+    }
 
-void single_cscan(Elevator& e) {
-
-}
-
-void single_clook(Elevator& e) {
-
+    // if there are no more floors to service, exit early
+    if (next_floor == -1) {
+        e.stopped = true;
+        return;
+    }
+    else {
+        e.next_floor = next_floor;
+    }
 }
--- src/elevator.h
+++ src/elevator.h
@@ -3,16 +3,19 @@
 #include <vector>
 
 enum class Direction {
-    up,
-    down
+    Up,
+    Down
 };
+
+Direction operator-(Direction& d);
 
 struct Elevator {
     const int min_floor = 0;
     const int max_floor;
     int current_floor = 0;
+    int next_floor = 0;
     bool stopped = true;
-    Direction move_dir = Direction::up;
+    Direction move_dir = Direction::Up;
     std::vector<bool> floors_called;
 
     // ctor
@@ -24,7 +27,9 @@ struct Elevator {
 
 void single_scan(Elevator& e);
 void single_look(Elevator& e);
-void single_cscan(Elevator& e);
-void single_clook(Elevator& e);
+
+// if this were a disk i might do these, but it's an elevator!
+//void single_cscan(Elevator& e);
+//void single_clook(Elevator& e);
 
 
